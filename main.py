bf30979f40c2c5f2a88cf75814bef52d79c166c9
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-06-19 12:44:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,286 +1,269 @@
-datax = {
-    "name": "ali",
-    "age": 4,
-    "is_male": True,
-    "is_femail": False,
-    "result": {
-        "have_sis": True,
-        "sis_result": {
-            "have_kos": True,
-            "size_of_kos": 43
-        }
-    },
-    1: 'kos',
-    2: True,
-    3: 6,
-    4: {
-        "kir": "kon"
-    }
-}
-
-# set - tuple
 
 import os
 
 os.system("")
 
-def TupleParse(data: tuple):
-    d = "\033[00m( "
-    num = 0
+class Parse(object):
+    def __init__(self) -> None:
+        pass
 
-    for item in data:
-        num += 1
+    def TupleParse(self, data: tuple):
+        d = "\033[00m( "
+        num = 0
 
-        if type(item) == str:
-            if len(data) - num == 0:
-                d += f"\033[32m'{item}'\033[00m "
-            else:
-                d += f"\033[32m'{item}'\033[00m, "
+        for item in data:
+            num += 1
 
-        if type(item) == int:
-            if len(data) - num == 0:
-                d += f"\033[93m{item}\033[00m "
-            else:
-                d += f"\033[93m{item}\033[00m, "
-
-        if type(item) == bool:
-            if len(data) - num == 0:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m "
+            if type(item) == str:
+                if len(data) - num == 0:
+                    d += f"\033[32m'{item}'\033[00m "
                 else:
-                    d += f"\033[91mfalse\033[00m "
+                    d += f"\033[32m'{item}'\033[00m, "
+
+            if type(item) == int:
+                if len(data) - num == 0:
+                    d += f"\033[93m{item}\033[00m "
+                else:
+                    d += f"\033[93m{item}\033[00m, "
+
+            if type(item) == bool:
+                if len(data) - num == 0:
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m "
+                    else:
+                        d += f"\033[91mfalse\033[00m "
+                
+                else:
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m, "
+                    else:
+                        d += f"\033[91mfalse\033[00m, "
+
+            if type(item) == tuple:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.TupleParse(item)}\033[00m "
+                else:
+                    d += f"\033[00m{self.TupleParse(item)}\033[00m "
+
+            if type(item) == dict:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.DictParse(item)}\033[00m "
+                else:
+                    d += f"\033[00m{self.DictParse(item)}\033[00m "
+
+            if type(item) == list:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.ListParse(item)}\033[00m "
+                else:
+                    d += f"\033[00m{self.ListParse(item)}\033[00m "
+
+            if type(item) == set:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.SetParse(item)}\033[00m "
+                else:
+                    d += f"\033[00m{self.SetParse(item)}\033[00m "
+
+        d += "\033[00m)"
+
+        return d
+
+    def SetParse(self, data: set):
+        d = "\033[00m{ "
+        num = 0
+
+        for item in data:
+            num += 1
             
-            else:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m, "
+            if type(item) == str:
+                if len(data) - num == 0:
+                    d += f"\033[32m'{item}'\033[00m "
                 else:
-                    d += f"\033[91mfalse\033[00m, "
+                    d += f"\033[32m'{item}'\033[00m, "
 
-        if type(item) == tuple:
-            if len(data) - num == 0:
-                d += f"\033[00m{TupleParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{TupleParse(item)}\033[00m "
-
-        if type(item) == dict:
-            if len(data) - num == 0:
-                d += f"\033[00m{DictParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{DictParse(item)}\033[00m "
-
-        if type(item) == list:
-            if len(data) - num == 0:
-                d += f"\033[00m{ListParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{ListParse(item)}\033[00m "
-
-        if type(item) == set:
-            if len(data) - num == 0:
-                d += f"\033[00m{SetParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{SetParse(item)}\033[00m "
-
-    d += "\033[00m)"
-
-    return d
-
-def SetParse(data: set):
-    d = "\033[00m{ "
-    num = 0
-
-    for item in data:
-        num += 1
-        
-        if type(item) == str:
-            if len(data) - num == 0:
-                d += f"\033[32m'{item}'\033[00m "
-            else:
-                d += f"\033[32m'{item}'\033[00m, "
-
-        if type(item) == int:
-            if len(data) - num == 0:
-                d += f"\033[93m{item}\033[00m "
-            else:
-                d += f"\033[93m{item}\033[00m, "
-
-        if type(item) == bool:
-            if len(data) - num == 0:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m "
+            if type(item) == int:
+                if len(data) - num == 0:
+                    d += f"\033[93m{item}\033[00m "
                 else:
-                    d += f"\033[91mfalse\033[00m "
+                    d += f"\033[93m{item}\033[00m, "
 
-            else:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m, "
+            if type(item) == bool:
+                if len(data) - num == 0:
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m "
+                    else:
+                        d += f"\033[91mfalse\033[00m "
+
                 else:
-                    d += f"\033[91mfalse\033[00m, "
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m, "
+                    else:
+                        d += f"\033[91mfalse\033[00m, "
 
-    d += " \033[00m}"
+        d += " \033[00m}"
 
-def ListParse(data: list):
-    d = "\033[00m[ "
-    num = 0
+    def ListParse(self, data: list):
+        d = "\033[00m[ "
+        num = 0
 
-    for item in data:
-        num += 1
+        for item in data:
+            num += 1
 
-        if type(item) == str:
-            if len(data) - num == 0:
-                d += f"\033[32m'{item}'\033[00m "
-            else:
-                d += f"\033[32m'{item}'\033[00m, "
-        
-        if type(item) == int:
-            if len(data) - num == 0:
-                d += f"\033[93m{item}\033[00m "
-            else:
-                d += f"\033[93m{item}\033[00m, "
-
-        if type(item) == bool:
-            if len(data) - num == 0:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m "
+            if type(item) == str:
+                if len(data) - num == 0:
+                    d += f"\033[32m'{item}'\033[00m "
                 else:
-                    d += f"\033[91mfalse\033[00m "
-
-            else:
-                if item == True:
-                    d += f"\033[94mtrue\033[00m, "
+                    d += f"\033[32m'{item}'\033[00m, "
+            
+            if type(item) == int:
+                if len(data) - num == 0:
+                    d += f"\033[93m{item}\033[00m "
                 else:
-                    d += f"\033[91mfalse\033[00m, "
+                    d += f"\033[93m{item}\033[00m, "
 
-        if type(item) == list:
-            if len(data) - num == 0:
-                d += f"\033[93m{ListParse(item)}\033[00m "
-            else:
-                d += f"\033[93m{ListParse(item)}\033[00m, "
+            if type(item) == bool:
+                if len(data) - num == 0:
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m "
+                    else:
+                        d += f"\033[91mfalse\033[00m "
 
-        if type(item) == dict:
-            if len(data) - num == 0:
-                d += f"\033[93m{DictParse(item)}\033[00m "
-            else:
-                d += f"\033[93m{DictParse(item)}\033[00m, "
-
-        if type(item) == tuple:
-            if len(data) - num == 0:
-                d += f"\033[00m{TupleParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{TupleParse(item)}\033[00m "
-
-        if type(item) == set:
-            if len(data) - num == 0:
-                d += f"\033[00m{SetParse(item)}\033[00m "
-            else:
-                d += f"\033[00m{SetParse(item)}\033[00m "
-
-    d += "\033[00m]"
-
-    return d
-
-def DictParse(data: dict):
-    d = "\033[00m{ "
-    num = 0
-
-    for key, val in data.items():
-        num += 1
-
-        if type(key) == str and type(val) == str:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}: \033[32m'{val}'\033[00m "
-            else:
-                d += f"\033[00m{key}: \033[32m'{val}'\033[00m, "
-
-        if type(key) == str and type(val) == int:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}: \033[93m{val}\033[00m "
-            else:
-                d += f"\033[00m{key}: \033[93m{val}\033[00m, "
-
-        if type(key) == str and type(val) == bool:
-            if len(data.keys()) - num == 0:
-                if str(val) == "True":
-                    d += f"\033[00m{key}: \033[94mtrue\033[00m "
                 else:
-                    d += f"\033[00m{key}: \033[91mfalse\033[00m "
-            else:
-                if str(val) == "True":
-                    d += f"\033[00m{key}: \033[94mtrue\033[00m, "
+                    if item == True:
+                        d += f"\033[94mtrue\033[00m, "
+                    else:
+                        d += f"\033[91mfalse\033[00m, "
+
+            if type(item) == list:
+                if len(data) - num == 0:
+                    d += f"\033[93m{self.ListParse(item)}\033[00m "
                 else:
-                    d += f"\033[00m{key}: \033[91mfalse\033[00m, "
+                    d += f"\033[93m{self.ListParse(item)}\033[00m, "
 
-        if type(key) == int and type(val) == str:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[32m'{val}'\033[00m "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[32m'{val}'\033[00m, "
-
-        if type(key) == int and type(val) == int:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[93m{val}\033[00m "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[93m{val}\033[00m, "
-
-        if type(key) == int and type(val) == bool:
-            if len(data.keys()) - num == 0:
-                if str(val) == "True":
-                    d += f"\033[93m{key}\033[00m: \033[94mtrue\033[00m "
+            if type(item) == dict:
+                if len(data) - num == 0:
+                    d += f"\033[93m{self.DictParse(item)}\033[00m "
                 else:
-                    d += f"\033[93m{key}\033[00m: \033[91mfalse\033[00m "
-            else:
-                if str(val) == "True":
-                    d += f"\033[93m{key}\033[00m: \033[94mtrue\033[00m, "
+                    d += f"\033[93m{self.DictParse(item)}\033[00m, "
+
+            if type(item) == tuple:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.TupleParse(item)}\033[00m "
                 else:
-                    d += f"\033[93m{key}\033[00m: \033[91mfalse\033[00m, "
+                    d += f"\033[00m{self.TupleParse(item)}\033[00m "
 
-        if type(key) == int and type(val) == dict:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[00m{DictParse(val)} "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[00m{DictParse(val)}, "
+            if type(item) == set:
+                if len(data) - num == 0:
+                    d += f"\033[00m{self.SetParse(item)}\033[00m "
+                else:
+                    d += f"\033[00m{self.SetParse(item)}\033[00m "
 
-        if type(key) == int and type(val) == tuple:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[00m{TupleParse(val)} "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[00m{TupleParse(val)}, "
+        d += "\033[00m]"
 
-        if type(key) == int and type(val) == set:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[00m{SetParse(val)} "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[00m{SetParse(val)}, "
+        return d
 
-        if type(key) == int and type(val) == list:
-            if len(data.keys()) - num == 0:
-                d += f"\033[93m{key}\033[00m: \033[00m{ListParse(val)} "
-            else:
-                d += f"\033[93m{key}\033[00m: \033[00m{ListParse(val)}, "
+    def DictParse(self, data: dict):
+        d = "\033[00m{ "
+        num = 0
 
-        if type(key) == str and type(val) == dict:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}\033[00m: \033[00m{DictParse(val)} "
-            else:
-                d += f"\033[00m{key}\033[00m: \033[00m{DictParse(val)}, "
+        for key, val in data.items():
+            num += 1
 
-        if type(key) == str and type(val) == tuple:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}\033[00m: \033[00m{TupleParse(val)} "
-            else:
-                d += f"\033[00m{key}\033[00m: \033[00m{TupleParse(val)}, "
+            if type(key) == str and type(val) == str:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}: \033[32m'{val}'\033[00m "
+                else:
+                    d += f"\033[00m{key}: \033[32m'{val}'\033[00m, "
 
-        if type(key) == str and type(val) == set:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}\033[00m: \033[00m{SetParse(val)} "
-            else:
-                d += f"\033[00m{key}\033[00m: \033[00m{SetParse(val)}, "
+            if type(key) == str and type(val) == int:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}: \033[93m{val}\033[00m "
+                else:
+                    d += f"\033[00m{key}: \033[93m{val}\033[00m, "
 
-        if type(key) == str and type(val) == list:
-            if len(data.keys()) - num == 0:
-                d += f"\033[00m{key}\033[00m: \033[00m{ListParse(val)} "
-            else:
-                d += f"\033[00m{key}\033[00m: \033[00m{ListParse(val)}, "
+            if type(key) == str and type(val) == bool:
+                if len(data.keys()) - num == 0:
+                    if str(val) == "True":
+                        d += f"\033[00m{key}: \033[94mtrue\033[00m "
+                    else:
+                        d += f"\033[00m{key}: \033[91mfalse\033[00m "
+                else:
+                    if str(val) == "True":
+                        d += f"\033[00m{key}: \033[94mtrue\033[00m, "
+                    else:
+                        d += f"\033[00m{key}: \033[91mfalse\033[00m, "
 
-    d += "\033[00m}"
+            if type(key) == int and type(val) == str:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[32m'{val}'\033[00m "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[32m'{val}'\033[00m, "
 
-    return d
+            if type(key) == int and type(val) == int:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[93m{val}\033[00m "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[93m{val}\033[00m, "
+
+            if type(key) == int and type(val) == bool:
+                if len(data.keys()) - num == 0:
+                    if str(val) == "True":
+                        d += f"\033[93m{key}\033[00m: \033[94mtrue\033[00m "
+                    else:
+                        d += f"\033[93m{key}\033[00m: \033[91mfalse\033[00m "
+                else:
+                    if str(val) == "True":
+                        d += f"\033[93m{key}\033[00m: \033[94mtrue\033[00m, "
+                    else:
+                        d += f"\033[93m{key}\033[00m: \033[91mfalse\033[00m, "
+
+            if type(key) == int and type(val) == dict:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.DictParse(val)} "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.DictParse(val)}, "
+
+            if type(key) == int and type(val) == tuple:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.TupleParse(val)} "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.TupleParse(val)}, "
+
+            if type(key) == int and type(val) == set:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.SetParse(val)} "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.SetParse(val)}, "
+
+            if type(key) == int and type(val) == list:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.ListParse(val)} "
+                else:
+                    d += f"\033[93m{key}\033[00m: \033[00m{self.ListParse(val)}, "
+
+            if type(key) == str and type(val) == dict:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.DictParse(val)} "
+                else:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.DictParse(val)}, "
+
+            if type(key) == str and type(val) == tuple:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.TupleParse(val)} "
+                else:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.TupleParse(val)}, "
+
+            if type(key) == str and type(val) == set:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.SetParse(val)} "
+                else:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.SetParse(val)}, "
+
+            if type(key) == str and type(val) == list:
+                if len(data.keys()) - num == 0:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.ListParse(val)} "
+                else:
+                    d += f"\033[00m{key}\033[00m: \033[00m{self.ListParse(val)}, "
+
+        d += "\033[00m}"
+
+        return d
